c8390f7be06d72a6b7b7a912c3f6d81a14e8ff80
Charles Randolph <c.x.randolph@student.tudelft.nl>
2020-03-16 01:36:51 +0100

    Some more work

--- include/ble_task.h
+++ include/ble_task.h
@@ -25,6 +25,7 @@
 #include "config.h"
 #include "errors.h"
 #include "signals.h"
+#include "ble.h"
 
 
 /*
--- src/ble_task.c
+++ src/ble_task.c
@@ -3,6 +3,40 @@
 
 /*
  *******************************************************************************
+ *                          Internal Global Variables                          *
+ *******************************************************************************
+*/
+
+
+// Queue holding incoming data from the BLE driver
+static xQueueHandle g_ble_rx_queue = NULL;
+
+
+// Queue holding data to transmit via the BLE driver
+static xQueueHandle g_ble_tx_queue = NULL;
+
+
+/*
+ *******************************************************************************
+ *                        Internal Function Definitions                        *
+ *******************************************************************************
+*/
+
+
+// Callback invoked when data arrives to the buffer
+esp_err_t ble_rx_cb (size_t size, void *buffer) {
+
+}
+
+
+// Callback type invoked on bluetooth state change
+esp_err_t ble_state_event_cb(ble_state_event_t state_event) {
+
+}
+
+
+/*
+ *******************************************************************************
  *                            Function Definitions                             *
  *******************************************************************************
 */
@@ -11,8 +45,23 @@
 void task_ble (void *args) {
 	esp_err_t err = ESP_OK;
 
+
+	// Initialize the RX queue
+	if ((g_ble_rx_queue = xQueueCreate()) == NULL) {
+		ERR("Unable to initialize RX queue!");
+		goto esc;
+	}
+
+	// Initialize the TX queue
+	if ((g_ble_tx_queue = xQueueCreate()) == NULL) {
+		ERR("Unable to initialize TX queue!");
+		goto esc;
+	}
+
 	// Run main loop
 	do {
+
+		// Wait for trigger
 
 
 	} while (1);
